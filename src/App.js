e714e4c14ca60631bace487f2edfdc6d9c052a20
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-09-08 01:38:08 -0400

    refactored UserCard to pull all data

--- src/App.js
+++ src/App.js
@@ -2,21 +2,10 @@ import React from "react";
 import UserCard from "./components/UserCard";
 
 class App extends React.Component {
-  state = { userName: "ajohnson1031", userData: "" };
-
-  componentDidMount() {
-    fetch("https://api.github.com/users/ajohnson1031")
-      .then(res => res.json())
-      .then(res => {
-        this.setState({ userName: this.state.userName, userData: res });
-      })
-      .catch(err => console.log(err));
-  }
-
   render() {
     return (
       <div className="App">
-        <UserCard props={this.state} />
+        <UserCard />
       </div>
     );
   }
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -5,36 +5,52 @@ import UserDetails from "./UserDetails";
 import FollowCard from "./FollowCard";
 import RepoList from "./RepoList";
 
-const UserCard = ({ props }) => {
-  const ud = props.userData;
+class UserCard extends React.Component {
+  state = { userName: "ajohnson1031", userData: "" };
 
-  const orangeTheme = {
-    background: "transparent",
-    text: "#FFF",
-    grade4: "#FF8F00",
-    grade3: "#FFA724",
-    grade2: "#FFC400",
-    grade1: "#ffd195",
-    grade0: "#FFF5E8"
-  };
+  componentDidMount() {
+    fetch("https://api.github.com/users/ajohnson1031")
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ userName: this.state.userName, userData: res });
+      })
+      .catch(err => console.log(err));
+  }
 
-  return (
-    ud && (
-      <Container className="main-wrapper">
-        <Card>
-          <Card.Header>
-            <h1>GitHub User Profile: {ud.name}</h1>
-          </Card.Header>
-          <Card.Content className="content user-calendar">
-            <GitHubCalendar username={ud.login} theme={orangeTheme} />
-          </Card.Content>
-          <UserDetails userDetails={ud} />
-          <FollowCard followers={ud.followers_url} />
-          <RepoList reposUrl={ud.repos_url} count={ud.public_repos} />
-        </Card>
-      </Container>
-    )
-  );
-};
+  render() {
+    const orangeTheme = {
+      background: "transparent",
+      text: "#FFF",
+      grade4: "#FF8F00",
+      grade3: "#FFA724",
+      grade2: "#FFC400",
+      grade1: "#ffd195",
+      grade0: "#FFF5E8"
+    };
+    return (
+      this.state.userData && (
+        <Container className="main-wrapper">
+          <Card>
+            <Card.Header>
+              <h1>GitHub User Profile: {this.state.userName}</h1>
+            </Card.Header>
+            <Card.Content className="content user-calendar">
+              <GitHubCalendar
+                username={this.state.userData.login}
+                theme={orangeTheme}
+              />
+            </Card.Content>
+            <UserDetails userDetails={this.state.userData} />
+            <FollowCard followers={this.state.userData.followers_url} />
+            <RepoList
+              reposUrl={this.state.userData.repos_url}
+              count={this.state.userData.public_repos}
+            />
+          </Card>
+        </Container>
+      )
+    );
+  }
+}
 
 export default UserCard;
