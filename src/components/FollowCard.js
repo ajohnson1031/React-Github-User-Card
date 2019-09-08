eb28bec849a8490420aaa0ead33181f9501fbd7f
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-09-07 23:38:49 -0400

    added followers component

---
+++ src/components/FollowCard.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Card, Image } from "semantic-ui-react";
+
+class FollowCard extends React.Component {
+  constructor({ followers }) {
+    super();
+    this.followers = followers;
+
+    this.state = {
+      followers_url: this.followers,
+      followers: "",
+      follower_imgs: ""
+    };
+  }
+
+  componentDidMount() {
+    // console.log(this.state);
+    fetch(this.state.followers_url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          followers_url: this.state.followers_url,
+          followers: res,
+          follower_imgs: res.map(r => r.avatar_url)
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <Card.Content className="mycontent following-content">
+        <div>
+          <h4>Followers ({this.state.followers.length}):</h4>
+        </div>
+        <div className="follower-list">
+          {this.state.followers &&
+            this.state.followers.map((follower, i) => {
+              return (
+                <a
+                  href={follower.html_url}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  key={follower.id}
+                >
+                  <div className="follower-tab">
+                    <Image src={this.state.follower_imgs[i]} />
+                    <p>{follower.login}</p>
+                  </div>
+                </a>
+              );
+            })}
+        </div>
+      </Card.Content>
+    );
+  }
+}
+export default FollowCard;
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,28 +1,21 @@
 import React from "react";
-import { Container, Card, Image, Icon } from "semantic-ui-react";
+import { Container, Card } from "semantic-ui-react";
 import GitHubCalendar from "react-github-calendar";
+import UserDetails from "./UserDetails";
+import FollowCard from "./FollowCard";
 
 const UserCard = ({ props }) => {
   const ud = props.userData;
-  console.log("UD LOGIN: " + ud.login);
-  const date = new Date(ud.created_at);
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-  const outputDate = `${
-    monthNames[date.getMonth()]
-  } ${date.getDate()}, ${date.getFullYear()}`;
+
+  const orangeTheme = {
+    background: "transparent",
+    text: "#FFF",
+    grade4: "#FF8F00",
+    grade3: "#FFA724",
+    grade2: "#FFC400",
+    grade1: "#ffd195",
+    grade0: "#FFF5E8"
+  };
 
   return (
     ud && (
@@ -32,43 +25,10 @@ const UserCard = ({ props }) => {
             <h1>GitHub User Profile: {ud.name}</h1>
           </Card.Header>
           <Card.Content className="content user-calendar">
-            <GitHubCalendar username={ud.login} color="lightorange" />
+            <GitHubCalendar username={ud.login} theme={orangeTheme} />
           </Card.Content>
-
-          <Card.Content className="content user-content">
-            <Card.Content className="img-detail">
-              <Image src={ud.avatar_url} alt={`${ud.name}'s Avatar`} />
-              <div className="img-name">
-                <a href={ud.html_url} target="_blank">
-                  {ud.login}
-                </a>
-              </div>
-            </Card.Content>
-            <Card.Content className="user-detail">
-              <h4>User Details</h4>
-              <ul>
-                <li>
-                  <strong>Date Joined:</strong> {outputDate}
-                </li>
-                <li>
-                  <strong>Repos:</strong> {ud.public_repos}
-                </li>
-                <li>
-                  <strong>Followers:</strong> {ud.followers}
-                </li>
-                <li>
-                  <strong>Following:</strong> {ud.following}
-                </li>
-              </ul>
-
-              <a
-                href="https://github.com/search?q=ajohnson1031&type=Users"
-                target="_blank"
-              >
-                <p className="follow-link">Follow</p>{" "}
-              </a>
-            </Card.Content>
-          </Card.Content>
+          <UserDetails userDetails={ud} />
+          <FollowCard followers={ud.followers_url} />
         </Card>
       </Container>
     )
